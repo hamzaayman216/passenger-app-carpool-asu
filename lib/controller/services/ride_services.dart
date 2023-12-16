6b7fe995798fb24a5a62c7e5728a666da19e96cd
hamzaayman216 <hamzaayman@live.com>
2023-12-16 10:59:49 +0200

    cleaning code

--- lib/controller/services/ride_services.dart
+++ lib/controller/services/ride_services.dart
@@ -2,20 +2,6 @@ import 'package:carpool/models/ride.dart';
 import 'package:firebase_database/firebase_database.dart';
 
 class RidesService {
-  List<Ride> getDriverRides(DatabaseEvent snapshot, String driverId) {
-    var ridesMap = snapshot.snapshot.value as Map<dynamic, dynamic>;
-    List<Ride> rides = [];
-    ridesMap.forEach((key, value) {
-      if (value is Map<dynamic, dynamic>) {
-        var rideMap = Map<String, dynamic>.from(value);
-        var ride = Ride.fromMap(rideMap);
-        if (ride.driverId == driverId && ride.status != 'Finished') {
-          rides.add(ride);
-        }
-      }
-    });
-    return rides;
-  }
 
   List<Ride> getHistoryRides(DatabaseEvent snapshot, String driverId) {
     var ridesMap = snapshot.snapshot.value as Map<dynamic, dynamic>;
