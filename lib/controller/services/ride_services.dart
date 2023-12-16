c615ad322a8142651cdf77eb77ff03a6a205ff36
hamzaayman216 <hamzaayman@live.com>
2023-12-16 11:49:50 +0200

    cleaning code

--- lib/controller/services/ride_services.dart
+++ lib/controller/services/ride_services.dart
@@ -1,16 +1,17 @@
+import 'package:carpool/components/cart_tile.dart';
 import 'package:carpool/models/ride.dart';
 import 'package:firebase_database/firebase_database.dart';
 
 class RidesService {
 
-  List<Ride> getHistoryRides(DatabaseEvent snapshot, String driverId) {
+  List<Ride> getHistoryRides(DatabaseEvent snapshot, String loggedInUser) {
     var ridesMap = snapshot.snapshot.value as Map<dynamic, dynamic>;
     List<Ride> rides = [];
     ridesMap.forEach((key, value) {
       if (value is Map<dynamic, dynamic>) {
         var rideMap = Map<String, dynamic>.from(value);
         var ride = Ride.fromMap(rideMap);
-        if (ride.driverId == driverId && ride.status == 'Finished') {
+        if (ride.passengerIds!.contains(loggedInUser) && ride.status == 'Finished') {
           rides.add(ride);
         }
       }
--- lib/screens/passenger_screen.dart
+++ lib/screens/passenger_screen.dart
@@ -5,6 +5,7 @@ import 'package:carpool/models/ride.dart';
 import 'package:carpool/models/user.dart';
 import 'package:carpool/screens/profile_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 late User loggedInUser;
@@ -41,6 +42,27 @@ class _PassengerScreenState extends State<PassengerScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Stream<List<Ride>> ridesStream = FirebaseDatabase.instance
+        .ref('rides')
+        .onValue
+        .map((event) {
+      if (event.snapshot.value != null) {
+        Map<dynamic, dynamic> data = event.snapshot.value as Map<dynamic, dynamic>;
+        List<Ride> rides = [];
+        data.forEach((key, value) {
+          if (value is Map<dynamic, dynamic>) {
+            Ride ride = Ride.fromMap(Map<String, dynamic>.from(value));
+            if (ride.driverId != loggedInUser.uid &&ride.status!='Finished') { // Use driverId for comparison
+              rides.add(ride);
+            }
+          }
+        });
+        return rides;
+      } else {
+        return <Ride>[];
+      }
+    });
+
 
 
     return WillPopScope(
